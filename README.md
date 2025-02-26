36a9b11ec2162c43add80929d2fee0ed5874bad9
Ian Nappi <ian.nappi@gwmail.gwu.edu>
2025-02-26 13:48:12 -0500

    initial WIP files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# satzbank.github.io
+# 🇩🇪 Satzbank
 Satzbank is a static webpage that stores German sentences to aid vocabulary learning in context. Users can input verbs, adjectives, nouns and more along with sentences that utilize them to practice writing and to improve retention. All data is cached locally and never saved to an external database.
+
+## ! Warning !
+Satzbank does not utilize an external database to store data. If you clear your browser cookies and site data on this web page, your content will not be saved!
---
+++ main.js
@@ -0,0 +1,24 @@
+let meisterWortListe;
+
+
+function verbenHinzufügen(event) {
+    event.preventDefault();
+
+    // Wortstring abrufen
+    let wortStr = document.getElementById("verbWortliste");
+
+    // let box = document.getElementById("verben");
+
+    // update html by inserting the word in the right order and writing all back to string
+    // e.g. sort list, then convert to string, then store back
+
+    // use localStorage.setItem("item", itemText); to store stuff locally
+
+    wortStr.value = "";
+}
+
+function nomenHinzufügen() {
+}
+
+function adjektiveHinzufügen() {
+}
